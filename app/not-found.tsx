b023e24d7f49d84a92a4b97e85a7373f149ac9e1
Shaun-Snyder <theverybestatthis@hotmail.com>
2025-12-17 09:01:33 -0500

    Delete app/not-found.tsx

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Not Found</h1>
-      <p>If you see this, routing is working.</p>
-    </main>
-  );
-}
