826dca1c572aec83fe63355a4eedb5e339fd6b5c
Shaun-Snyder <theverybestatthis@hotmail.com>
2025-12-17 06:30:18 -0500

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Not Found</h1>
+      <p>If you see this, routing is working.</p>
+    </main>
+  );
+}
